bd61dcfec3cda56d42c3260ad646d0c18b83676a
Pavel Sakhanko <iospaveldev@gmail.com>
2021-01-17 16:03:47 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Pavel Sakhanko on 17.01.21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
